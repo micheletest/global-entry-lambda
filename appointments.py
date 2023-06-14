e19cd04acbd6921cca447034de3a088319c7bbed
Michele Martone <michele.martone@gmail.com>
2023-06-14 11:12:33 -0400

    add to the README. Implement the twilio messages.create

--- appointments.py
+++ appointments.py
@@ -11,7 +11,8 @@ PREFERRED_TIMESLOTS = [{'StartTime': datetime.strptime('Jun 12 2023', '%b %d %Y'
                         'EndTime': datetime.strptime('Jul 26 2023', '%b %d %Y')}]
 TWILIO_TEXT_TO = os.getenv('TWILIO_TEXT_TO')
 TWILIO_TEXT_FROM = os.getenv('TWILIO_TEXT_FROM')
-# twilio_client = Client()
+twilio_client = Client(os.getenv('TWILIO_ACCOUNT_SID'),
+                       os.getenv('TWILIO_AUTH_TOKEN'))
 
 
 def parse_timeslot_datetime(timeslot: dict) -> dict:
@@ -26,22 +27,21 @@ def get_timeslots_for_location(location_id):
 
 
 def check_for_great_timeslots(timeslots):
+    good_timeslots = []
     for timeslot in timeslots:
         if (timeslot > PREFERRED_TIMESLOTS[0]['StartTime'] and timeslot < PREFERRED_TIMESLOTS[0]['EndTime']) or (timeslot > PREFERRED_TIMESLOTS[1]['StartTime'] and timeslot < PREFERRED_TIMESLOTS[1]['EndTime']):
-            print('got a good timeslot')
-        else:
-            print('not')
+            good_timeslots.append(timeslot)
+    return good_timeslots
 
 
-def handler(event, context):
-    print('main')
+def lambda_handler(event, context):
     for location_id in LOCATION_IDS:
         timeslots = get_timeslots_for_location(location_id)
-        print(timeslots)
         good_timeslot = check_for_great_timeslots(timeslots)
-
-    # if ochd_home == 0 or ochd_covid_page == 0:
-    #     message = twilio_client.messages.create(to=TWILIO_TEXT_TO, from_=TWILIO_TEXT_FROM,
-    #                                             body=f'POTENTIAL AVAILABILITY! home:{ochd_home}, covid:{ochd_covid_page} {OCHD_MAIN_URL}')
-    # else:
-    #     print('Nothing to see here')
+        if good_timeslot:
+            print(
+                f'POTENTIAL AVAILABILITY! location_id:{location_id}, timeslot:{good_timeslot}')
+            message = twilio_client.messages.create(to=os.getenv('TWILIO_TEXT_TO'), from_=os.getenv('TWILIO_TEXT_FROM'),
+                                                    body=f'POTENTIAL AVAILABILITY! location_id:{location_id}, timeslot:{good_timeslot}')
+        else:
+            print('Nothing to see here')
